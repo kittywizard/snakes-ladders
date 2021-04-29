877fbd699648ce088bc7588db216e7294b7c075e
marleigh <volcana@gmail.com>
2021-04-29 16:12:30 -0700

    the most basic of grids

--- script.js
+++ script.js
@@ -2,15 +2,25 @@
 
 const grid = document.getElementById('grid');
 
+let arr = [];
+const gridLength = 55;
+
 function createGrid() {
-    //create an array
-    let arr = [];
-    for(let i = 0; i<55; i++) {
+    for(let i = 0; i < gridLength; i++) {
         let div = document.createElement('div');
-        
+        div.classList.add('square');
+        arr.push(div);
     }
+
+    console.log(arr);
+
+    arr.forEach(square => {
+        grid.appendChild(square);
+    })
 
     //create the array and for 55 times, create a div and push it to the array
     //apply any classes for styling
     //
 }
+
+createGrid();
