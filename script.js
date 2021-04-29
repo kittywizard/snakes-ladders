bcbb2f4986fdcc62e93c86d8f3be625025d9ab0c
marleigh <volcana@gmail.com>
2021-04-29 09:45:53 -0700

    initial commit with comments on how to build

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //create a grid 1-55
+
+const grid = document.getElementById('grid');
+
+function createGrid() {
+    //create an array
+    let arr = [];
+    for(let i = 0; i<55; i++) {
+        let div = document.createElement('div');
+        
+    }
+
+    //create the array and for 55 times, create a div and push it to the array
+    //apply any classes for styling
+    //
+}
