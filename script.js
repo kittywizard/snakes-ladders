a6b2f64068eb808a8d31f4a94e512dd778da4f87
marleigh <volcana@gmail.com>
2021-05-03 18:23:13 -0700

    added dice rolling, random player start

--- script.js
+++ script.js
@@ -2,17 +2,25 @@
 
 const grid = document.getElementById('grid');
 const scoreBoard = document.querySelector('.dice');
-const rollBtn = document.getElementById('rollBtn');
-const startBtn = document.getElementById('startBtn');
 const p1Score = document.getElementById('p1-score');
 const p2Score = document.getElementById('p2-score');
+const btnDisplay = document.querySelector('.player-display');
 
 let arr = [];
 const gridLength = 55;
+let currentPlayer = true; //where p1 = true, p2 = false
 
+//variables for the dice
+const min = 1;
+const max = 6;
+
+//event listeners and buttons
+const rollBtn = document.getElementById('rollBtn');
+const startBtn = document.getElementById('startBtn');
 rollBtn.addEventListener('click', rollDice);
 startBtn.addEventListener('click', createGrid);
 
+//function to create the grid, initialized when the button is clicked
 function createGrid() {
     startBtn.style.visibility = 'hidden';
     scoreBoard.style.display = 'inline-block';
@@ -27,7 +35,50 @@ function createGrid() {
     arr.forEach(square => {
         grid.appendChild(square);
     });
-}
-function rollDice() {
 
+    //display which player goes first
+    let flip = Math.floor((Math.random() * 2) + 1); //flip a coin, essentially
+    if(flip === 1){
+        //player 1 goes first, currentPlayer stays the same
+        btnDisplay.textContent = `Player 1`;
+    } else {
+        //player 2 goes first, currentPlayer becomes false
+        btnDisplay.textContent = `Player 2`;
+        currentPlayer = false;
+    }
+}
+
+function playerDetermine() {
+    if(currentPlayer) {
+        btnDisplay.textContent = `Player 1`;
+    } else {
+        btnDisplay.textContent = `Player 2`;
+    }
+}
+
+//rolls the dice when button is clicked.
+//should probably hide the dice while the rest takes place - then switch player. different colors maybe?
+
+function rollDice() {
+    //generate a random number between 1 and 6
+    let dice = Math.floor((Math.random() * max) + min);
+
+    //create a modal
+    const modal = document.createElement('div');
+    modal.classList.add('modal');
+    modal.textContent = `${dice}`;
+    scoreBoard.appendChild(modal);
+
+    //start the 'move' function
+    movePiece(currentPlayer, dice);
+}
+
+function movePiece(player, num) {
+    //determine which player is up (pass in var??)
+    //get dice roll result
+    //move piece x of spaces
+    //check to see if player has landed on a special snake or ladder piece
+    //pop up modal to let them know what happened
+
+    console.log(`${player} and ${num}`)
 }
